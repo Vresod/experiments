5e7452dd1caa534e53bf1777a1b6b3b962fd4912
Vresod <40863375+Vresod@users.noreply.github.com>
2018-10-07 16:50:39 -0400

    ???

---
+++ connect4/js/nav.js
@@ -0,0 +1,9 @@
+function next() {/*function for going forward*/
+	var slide = slide + 1;
+	document.getElementById('content').innerHTML =
+}
+
+function back() {/*function for going backward*/
+	var slide = slide - 1;
+	document.getElementById('content').innerHTML =
+}
---
+++ connect4/js/var.js
@@ -0,0 +1,5 @@
+var content1 = 'This book is a very good book. I would recommend it 10/10.';
+
+var content2 = '';
+
+var slide = 1;
